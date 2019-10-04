c3a903ceb862e01392a0a77a628d91cfa97bd935
GaboGongora <51409837+GaboGongora@users.noreply.github.com>
2019-10-04 08:40:11 -0400

    Add files via upload

---
+++ EIRAInstalacion.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt-get install python2.7
+sudo apt install python-numpy
+
+sudo apt install python-scipy
+
+sudo apt install python-matplotlib
+
+sudo apt install python-sklearn
+
+sudo apt install python-skimage
+
+sudo apt install python-opencv
+
+sudo apt install python-pandas
+
+apt-cache search pyqt
+
+sudo apt-get install python-qt4
+
+sudo apt install mysql-server
+
+sudo apt install apache2
+
